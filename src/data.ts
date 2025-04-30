dd1d3be6e3970f86efda2b2427c57f1f09a3c6c7
Inshiya Ravat <inshiya.ravat@simformsolutions.com>
2025-04-30 11:45:53 +0530

    added: data for crossword

---
+++ src/data.ts
@@ -0,0 +1,262 @@
+export const data= [
+        {
+            "word": "marie curie",
+            "clue": "French-Polish scientist who used to carry radium in her pocket",
+            "row": 0,
+            "col": 0,
+            "dir": "horizontal",
+            "linkedTo": []
+        },
+        {
+            "word": "nasa",
+            "clue": "American space program, abbr.",
+            "row": 0,
+            "col": 11,
+            "dir": "horizontal",
+            "linkedTo": []
+        },
+        {
+            "word": "braille",
+            "clue": "A way for the visually impaired to read",
+            "row": 2,
+            "col": 2,
+            "dir": "horizontal",
+            "linkedTo": []
+        },
+        {
+            "word": "media",
+            "clue": "Marshall McLuhan's main field of study",
+            "row": 2,
+            "col": 10,
+            "dir": "horizontal",
+            "linkedTo": []
+        },
+        {
+            "word": "tele",
+            "clue": "Prefix for vision or phone",
+            "row": 4,
+            "col": 2,
+            "dir": "horizontal",
+            "linkedTo": []
+        },
+        {
+            "word": "computer",
+            "clue": "What once filled an entire room now fits in our pockets",
+            "row": 4,
+            "col": 7,
+            "dir": "horizontal",
+            "linkedTo": []
+        },
+        {
+            "word": "matter",
+            "clue": "Dark ______, substance which occupies most of [9E-D]",
+            "row": 6,
+            "col": 0,
+            "dir": "horizontal",
+            "linkedTo": [
+                {
+                    "row": 8,
+                    "col": 4,
+                    "dir": "vertical"
+                }
+            ]
+        },
+        {
+            "word": "neutral",
+            "clue": "State of an atom with an equal number of electrons and protons",
+            "row": 6,
+            "col": 8,
+            "dir": "horizontal",
+            "linkedTo": []
+        },
+        {
+            "word": "sms",
+            "clue": "Phone message, abbr.",
+            "row": 8,
+            "col": 0,
+            "dir": "horizontal",
+            "linkedTo": []
+        },
+        {
+            "word": "smithsonian",
+            "clue": "American's largest museum complex",
+            "row": 8,
+            "col": 4,
+            "dir": "horizontal",
+            "linkedTo": []
+        },
+        {
+            "word": "ada lovelace",
+            "clue": "English mathematician considered to be the first computer programmer",
+            "row": 10,
+            "col": 2,
+            "dir": "horizontal",
+            "linkedTo": []
+        },
+        {
+            "word": "mine kafon",
+            "clue": "Autonomous wind-powered minesweeper",
+            "row": 12,
+            "col": 1,
+            "dir": "horizontal",
+            "linkedTo": []
+        },
+        {
+            "word": "mole",
+            "clue": "The basic unit of measurement for substance",
+            "row": 12,
+            "col": 11,
+            "dir": "horizontal",
+            "linkedTo": []
+        },
+        {
+            "word": "sonar",
+            "clue": "Sound-based navigation method",
+            "row": 14,
+            "col": 0,
+            "dir": "horizontal",
+            "linkedTo": []
+        },
+        {
+            "word": "fiction",
+            "clue": "A genre of literature based on futuristic imagination",
+            "row": 14,
+            "col": 8,
+            "dir": "horizontal",
+            "linkedTo": []
+        },
+        {
+            "word": "mae c jemison",
+            "clue": "First black woman to travel into space",
+            "row": 0,
+            "col": 0,
+            "dir": "vertical",
+            "linkedTo": []
+        },
+        {
+            "word": "robot",
+            "clue": "Computer programmed machine",
+            "row": 0,
+            "col": 2,
+            "dir": "vertical",
+            "linkedTo": []
+        },
+        {
+            "word": "email",
+            "clue": "Computer message",
+            "row": 0,
+            "col": 4,
+            "dir": "vertical",
+            "linkedTo": []
+        },
+        {
+            "word": "url",
+            "clue": "Web browser entry, abbr.",
+            "row": 0,
+            "col": 6,
+            "dir": "vertical",
+            "linkedTo": []
+        },
+        {
+            "word": "add",
+            "clue": "One of the four basic operations of math",
+            "row": 0,
+            "col": 12,
+            "dir": "vertical",
+            "linkedTo": []
+        },
+        {
+            "word": "lock",
+            "clue": "What a key opens",
+            "row": 2,
+            "col": 7,
+            "dir": "vertical",
+            "linkedTo": []
+        },
+        {
+            "word": "airplane",
+            "clue": "The Wrights Brothers' invention",
+            "row": 2,
+            "col": 14,
+            "dir": "vertical",
+            "linkedTo": []
+        },
+        {
+            "word": "pluto",
+            "clue": "Celestial body orbiting the sun, demoted to a dwarf planet in 2006",
+            "row": 4,
+            "col": 10,
+            "dir": "vertical",
+            "linkedTo": []
+        },
+        {
+            "word": "turbine",
+            "clue": "Key component in wind power plants",
+            "row": 4,
+            "col": 12,
+            "dir": "vertical",
+            "linkedTo": []
+        },
+        {
+            "word": "kilowatt",
+            "clue": "Unit of measurement for electrical power",
+            "row": 7,
+            "col": 6,
+            "dir": "vertical",
+            "linkedTo": []
+        },
+        {
+            "word": "station",
+            "clue": "Terminal for trains and buses - there's even one in [9E-D]",
+            "row": 8,
+            "col": 2,
+            "dir": "vertical",
+            "linkedTo": [
+                {
+                    "row": 8,
+                    "col": 4,
+                    "dir": "vertical"
+                }
+            ]
+        },
+        {
+            "word": "space",
+            "clue": "Where [7A-A], [5K-D] and [11L-D] come from",
+            "row": 8,
+            "col": 4,
+            "dir": "vertical",
+            "linkedTo": [
+                {
+                    "row": 6,
+                    "col": 0,
+                    "dir": "horizontal"
+                },
+                {
+                    "row": 4,
+                    "col": 10,
+                    "dir": "vertical"
+                },
+                {
+                    "row": 10,
+                    "col": 11,
+                    "dir": "vertical"
+                }
+            ]
+        },
+        {
+            "word": "comet",
+            "clue": "Interstellar traveller with a shiny tail",
+            "row": 10,
+            "col": 11,
+            "dir": "vertical",
+            "linkedTo": []
+        },
+        {
+            "word": "cern",
+            "clue": "Home to the Large Hadron Collider, abbr.",
+            "row": 11,
+            "col": 14,
+            "dir": "vertical",
+            "linkedTo": []
+        }
+    ]
